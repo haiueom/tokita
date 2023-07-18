8ad95694950c344820913a6e79061a6f463b65f9
Ilham Taufiq <65374686+haiueom@users.noreply.github.com>
2023-07-18 15:41:20 +0700

    Update order.tsx

--- components/buttons/order.tsx
+++ components/buttons/order.tsx
@@ -20,7 +20,7 @@ export default function OrderBtn({ text, product } : ButtonProps) {
             loading={loading}
             icon={ArrowUpRightIcon}
             onClick={() => {
-                router.push(`https://api.whatsapp.com/send?phone=6285792549522&text=Halo%20admin%2C%20saya%20mau%20order!%0ANama%3A%20%0ANama%20Barang%3A%20A${product.name}%0AHarga%3A%20${product.price}%0A%0ATerima%20kasih%20%F0%9F%98%8A}`)
+                router.push(`https://api.whatsapp.com/send?phone=6285792549522&text=Halo%20admin%2C%20saya%20mau%20order!%0A%0ANama%3A%20%0ANama%20Barang%3A%20${product.name}%0AHarga%3A%20${product.price}`)
                 setLoading(true);
             }}
         >
