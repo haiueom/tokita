d378a5d8208e13f0a86ef6b927797ecfc537094a
haiueom <ilhamtaufiq859@gmail.com>
2023-07-20 11:23:28 +0700

    update halaman admin dashboard

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,12 +1,21 @@
-import AllTable from '@/components/tables/all.user';
+import AllCategory from '@/components/tables/category/all';
+import AllProduct from '@/components/tables/product/all';
+import AllRole from '@/components/tables/role/all';
+import AllUser from '@/components/tables/user/all';
 import { Title, Text } from '@tremor/react';
 
 export default async function IndexPage() {
     return (
         <main className="mx-auto max-w-7xl p-4 md:p-10">
-            <Title>Users</Title>
-            <Text>A list of users retrieved from a MySQL database (PlanetScale).</Text>
-            <AllTable />
+            <Title>Dashboard Page</Title>
+            <Text>Welcome to the dashboard.</Text>
+            <AllProduct />
+            <Title className='mt-10'>Category</Title>
+            <AllCategory />
+            <Title className='mt-10'>User</Title>
+            <AllUser />
+            <Title className='mt-10'>Role</Title>
+            <AllRole />
         </main>
     );
 }
